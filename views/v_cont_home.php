01fffb349532c11ca44418cf57332b7d5d3ddf91
Garry Jane <mmm53.ripe@gmail.com>
2017-09-08 16:24:23 +0300

    сделан шаблон категирий, выгрузка проверочного массива категорий

--- views/v_cont_home.php
+++ views/v_cont_home.php
@@ -1,42 +1,66 @@
-    <div class="main">
-        <div class="row">
-            <div class="col-xs-12">
-                <h1 class="page-title__text">Коллекция растений Марины Григорьевой</h1>
-                <h2 class="page-title__text">+7 123 456 78 90</h2>
-            </div>
+<div class="main">
+    <div class="row">
+        <div class="col-xs-12">
+            <h1 class="page-title__text">Коллекция растений Марины Григорьевой</h1>
+            <h2 class="page-title__text">+7 123 456 78 90</h2>
         </div>
+    </div>
 
-        <div class="content">
+    <div class="content">
+        <div class="row">
 
-            <div class="row">
-                <div class="col-md-3 col-sm-6">
-                    <figure class="category-represent">
-                        <img src="img/plant-1.png" class="category-represent__img" alt=""/>
-                        <figcaption class="category-represent__name">Лилейники</figcaption>
-                    </figure>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <figure class="category-represent">
-                        <img src="img/plant-2.png" class="category-represent__img" alt=""/>
-                        <figcaption class="category-represent__name">Хосты</figcaption>
-                    </figure>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <figure class="category-represent">
-                        <img src="img/plant-3.png" class="category-represent__img" alt=""/>
-                        <figcaption class="category-represent__name">Пионы</figcaption>
-                    </figure>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <figure class="category-represent">
-                        <img src="img/plant-1.png" class="category-represent__img" alt=""/>
-                        <figcaption class="category-represent__name">Ирисы сибирские</figcaption>
-                    </figure>
-                </div>
-            </div>
+        <?php
+    /* отладочный массив данных, имитация DB*/
+    $categories = [
+        [
+            'cat_name' => 'Лилейники',
+            'cat_img' => 'img/plant-1.png'
+        ],
+        [
+            'cat_name' => 'Хосты',
+            'cat_img' => 'img/plant-2.png'
+        ],
+        [
+            'cat_name' => 'Пионы',
+            'cat_img' => 'img/plant-3.png'
+        ],
+        [
+            'cat_name' => 'Ирисы сибирские',
+            'cat_img' => 'img/plant-4.png'
+        ],
+        [
+            'cat_name' => 'Лилейники',
+            'cat_img' => 'img/plant-1.png'
+        ],
+        [
+            'cat_name' => 'Хосты',
+            'cat_img' => 'img/plant-2.png'
+        ],
+        [
+            'cat_name' => 'Пионы',
+            'cat_img' => 'img/plant-3.png'
+        ],
+        [
+            'cat_name' => 'Ирисы сибирские',
+            'cat_img' => 'img/plant-4.png'
+        ]
+    ];
 
-
-
-        </div> <!-- ./content -->
-    </div> <!-- ./main -->
+    $i = 0;
+    foreach ($categories as $cat) {
+        if( $i > 0 && $i % 4 == 0) {
+            echo "</div><div class='row'>";
+        }
+        echo "<div class='col-md-3 col-sm-6'>";
+        echo "<figure class='category-represent'>";
+        echo "<img src='{$cat['cat_img']}' class='category-represent__img' alt=''>";
+        echo "<figcaption class='category-represent__name'>{$cat['cat_name']}</figcaption>";
+        echo "</figure>";
+        echo "</div>";
+        $i++;
+    }
+    ?>
+        </div><!-- ./row-->
+    </div> <!-- ./content -->
+</div> <!-- ./main -->
 </div> <!-- ./container -->
